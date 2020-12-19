2c928e8dc12a73e844b0afe71829397d31086ab2
Auzzzz <s3544232@student.rmit.edu.au>
2020-12-19 11:31:12 +1100

    Initial Commit

---
+++ Messenger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by Chris Kelly on 19/12/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
